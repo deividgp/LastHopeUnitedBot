5f4fa58353abbaa34caa53436f104679c694e30f
deividgp <dayd4@outlook.com>
2021-08-09 09:40:36 +0200

    last commit before v13

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const Twit = require('twit');
+const http = require('https');
 require('dotenv').config()
 const {
   token
 } = require(`../config/${process.env.MODE}.json`);
+const {
+  isReply,
+} = require('./functions.js');
 const ListTrials = require('./trials/listTrials.js');
 const trials = new ListTrials();
 const T = new Twit({
@@ -18,9 +22,9 @@ client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler => {
-  if(handler == "event_handler"){
+  if (handler == "event_handler") {
     require(`./handlers/${handler}`)(client, trials);
-  }else{
+  } else {
     require(`./handlers/${handler}`)(client);
   }
 })
@@ -29,12 +33,37 @@ const stream = T.stream('statuses/filter', { follow: '718475378751381504' });
 
 stream.on('tweet', function (tweet) {
   let content = tweet.text;
-  if (content.includes("ESO") && content.includes("European PC")) {
+  if (content.includes("ESO") && content.includes("European PC") && !isReply(tweet)) {
     client.channels.fetch('811948277461024838')
       .then(channel => {
         channel.send(tweet.text);
       })
   }
 });
+
+/*setInterval(function () {
+  http.get("https://live-services.elderscrollsonline.com/status/realms", function (res) {
+    let data = '',
+      json_data;
+
+    res.on('data', function (stream) {
+      data += stream;
+    });
+    res.on('end', function () {
+      json_data = JSON.parse(data);
+      let serverInfo = json_data["zos_platform_response"]["response"];
+
+      for (let server in serverInfo) {
+        if (){
+          
+        }
+        serverEmbed.addField(`${server.substring(26, server.length - 1)}`, `${serverInfo[server]}`, true)
+      }
+
+      console
+    });
+  });
+
+}, 1000);*/
 
 client.login(token);
--- src/functions.js
+++ src/functions.js
@@ -122,8 +122,21 @@ const confirmTrial = async (channel, client) => {
     });
 }
 
+const isReply = (tweet) => {
+    if (tweet.retweeted_status
+        || tweet.in_reply_to_status_id
+        || tweet.in_reply_to_status_id_str
+        || tweet.in_reply_to_user_id
+        || tweet.in_reply_to_user_id_str
+        || tweet.in_reply_to_screen_name)
+        return true;
+
+    return false;
+}
+
 module.exports = {
     assignRole,
     deleteMessages,
-    confirmTrial
+    confirmTrial,
+    isReply
 }
