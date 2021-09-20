ac02c25386c2115d9b87221f2ac408bab44e828b
David Gutiérrez <david.gutierrez@estudiants.urv.cat>
2021-09-20 11:31:41 +0200

    v2.2.3

--- src/bot/commands/image.js
+++ src/bot/commands/image.js
@@ -1,50 +1,52 @@
-// const { ApplicationCommandOptionType } = require('discord-api-types/v9');
-// const htmlEntities = require('html-entities');
-// const url = require('url');
+const { ApplicationCommandOptionType } = require('discord-api-types/v9');
+const fetch = require('node-fetch');
 
-// module.exports = {
-//     name: 'image',
-//     description: 'Returns a random image',
-//     options: [{
-//         name: 'type',
-//         type: ApplicationCommandOptionType.String,
-//         description: 'Image type',
-//         required: true,
-//     }],
-//     async execute(trials, client, interaction) {
-//         image(interaction, interaction.options.getString('type'));
-//     }
-// }
+module.exports = {
+    name: 'image',
+    description: 'Returns a random image',
+    options: [{
+        name: 'type',
+        type: ApplicationCommandOptionType.String,
+        description: 'Image type',
+        required: true,
+    }],
+    async execute(trials, client, interaction) {
+        image(interaction, interaction.options.getString('type'));
+    }
+}
 
-// const image = (interaction, srch) => {
-//     /*var options = {
-//         url: 'http://results.dogpile.com/serp?qc=images&q=' + srch,
-//         method: 'GET',
-//         headers: {
-//             'Accept': 'text/html',
-//             'User-Agent': 'Chrome'
-//         }
-//     };
-//     request(options, async function (error, response, responseBody) {
-//         if (error) {
-//             return await interaction.reply({ content: "Error", ephemeral: true });
-//         }
-//         $ = cheerio.load(responseBody);
-//         var links = $('.image a.link');
-//         var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr('href'));
-//         console.log(urls);
-//         if (!urls.length) {
-//             return await interaction.reply({ content: "Error", ephemeral: true });
-//         }
-//         return await interaction.reply(urls[Math.floor(Math.random() * urls.length)]);
-//     });*/
-//     cheerio.fetch("https://www.google.com/search", { q: srch, tbm: 'isch', tbs: 'isz:m', safe: 'active' }, async (err, $, res) => {
-//         if (err) {
-//             return await interaction.reply({ content: "Error", ephemeral: true });
-//         }
-//         let urls = $('.rg_l').map((index, element) => {
-//             return url.parse(htmlEntities.decode($(element).attr('href')), true).query.imgurl
-//         });
-//         console.log(urls);
-//     })
-// }
+const image = async (interaction, srch) => {
+    const response = await fetch('https://www.google.com/search?q=has');
+    const body = await response.text();
+    console.log(body);
+    /*var options = {
+        url: 'http://results.dogpile.com/serp?qc=images&q=' + srch,
+        method: 'GET',
+        headers: {
+            'Accept': 'text/html',
+            'User-Agent': 'Chrome'
+        }
+    };
+    request(options, async function (error, response, responseBody) {
+        if (error) {
+            return await interaction.reply({ content: "Error", ephemeral: true });
+        }
+        $ = cheerio.load(responseBody);
+        var links = $('.image a.link');
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr('href'));
+        console.log(urls);
+        if (!urls.length) {
+            return await interaction.reply({ content: "Error", ephemeral: true });
+        }
+        return await interaction.reply(urls[Math.floor(Math.random() * urls.length)]);
+    });*/
+    // cheerio.fetch("https://www.google.com/search", { q: srch, tbm: 'isch', tbs: 'isz:m', safe: 'active' }, async (err, $, res) => {
+    //     if (err) {
+    //         return await interaction.reply({ content: "Error", ephemeral: true });
+    //     }
+    //     let urls = $('.rg_l').map((index, element) => {
+    //         return url.parse(htmlEntities.decode($(element).attr('href')), true).query.imgurl
+    //     });
+    //     console.log(urls);
+    // })
+}
--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -19,18 +19,18 @@ module.exports = {
         }, 4000);
       })
       .catch(console.error);*/
-    client.channels.fetch('811207960701042713')
-      .then(channel => {
-        channel.bulkDelete(6);
-        setTimeout(async function () {
-          await assignRole(channel, "811207960671813646");
-        }, 4000);
-        setTimeout(async function () {
-          await assignRole(channel, "811207960691867663");
-        }, 4000);
+    // client.channels.fetch('811207960701042713')
+    //   .then(channel => {
+    //     channel.bulkDelete(6);
+    //     setTimeout(async function () {
+    //       await assignRole(channel, "811207960671813646");
+    //     }, 4000);
+    //     setTimeout(async function () {
+    //       await assignRole(channel, "811207960691867663");
+    //     }, 4000);
 
-      })
-      .catch(console.error);
+    //   })
+    //   .catch(console.error);
 
     deploy(client);
   }
