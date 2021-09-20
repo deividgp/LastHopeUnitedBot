409760271ac4662e6369ffd0dd4473290a878641
deividgp <dayd4@outlook.com>
2021-09-20 09:08:30 +0200

    v2.2.3

--- src/bot/commands/confirmtrial.js
+++ src/bot/commands/confirmtrial.js
@@ -1,6 +1,3 @@
-const {
-    confirmTrial,
-} = require('../functions.js');
 const wait = require('util').promisify(setTimeout);
 
 module.exports = {
@@ -17,4 +14,55 @@ module.exports = {
                 confirmTrial(interaction, client);
             });
     }
+}
+
+const confirmTrial = async (interaction, client) => {
+    interaction.channel.awaitMessages({ max: 1, time: 15000 }).then(async collected => {
+
+        if (collected.first().content != "cancel") {
+            collected.first().delete();
+            const message = await interaction.channel.send(collected.first().content);
+            let messageContent = message.content;
+            let indexIds = 0;
+            let idsConfirmation = [];
+
+            for (let index = 0; index < messageContent.length; index++) {
+                if (messageContent.charAt(index) == '<') {
+                    idsConfirmation[indexIds] = messageContent.substring(index + 3, (index + 3) + 18);
+                    indexIds++;
+                    index = (index + 3) + 19;
+                }
+            }
+
+            await message.react('✔️');
+            await message.react('❌');
+            const filter = (reaction, user) => {
+
+                if ((message.member.id == user.id && reaction.emoji.name == '🛑') || (idsConfirmation.includes(user.id) && user.id != client.user.id && (reaction.emoji.name === '✔️' || reaction.emoji.name === '❌'))) {
+                    return true;
+                }
+
+                return false;
+            };
+
+            const collector = message.createReactionCollector({ filter });
+
+            collector.on('collect', (reaction, user) => {
+                if (reaction.emoji.name != '🛑') {
+                    const index = messageContent.search(user.id);
+                    messageContent = messageContent.replaceAt(index + 19, reaction.emoji.name);
+                    message.edit(messageContent);
+
+                } else {
+                    collector.stop('Collector stopped');
+                }
+            });
+
+        } else {
+            interaction.channel.send("Operation cancelled");
+        }
+
+    }).catch(() => {
+        interaction.channel.send('Operation canceled.');
+    });
 }
